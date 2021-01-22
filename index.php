006f40f373586066867184d68668a99de5545eba
Yorick Hoorneman <yorick@schubergphilis.com>
2021-01-22 16:17:47 +0100

    text change

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello World!";
+echo "Chief Justice Roberts, Vice President Harris, Speaker Pelosi, Leader Schumer, Leader McConnell, Vice President Pence, distinguished guests, and my fellow Americans.";
